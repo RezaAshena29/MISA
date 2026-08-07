0af292b25ac80285c5d67338a3375335dc983b0d
reza ashena <Ashenre@mfcgd.com>
2026-08-07 16:18:16 -0400

    Update assembly informational versions and cache files across multiple projects

--- src/MISA.Application/obj/Debug/net10.0/MISA.Application.AssemblyInfo.cs
+++ src/MISA.Application/obj/Debug/net10.0/MISA.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MISA.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+62b2e6c2e92de4fd46c1a0b0b9c49682de2b3753")]
 [assembly: System.Reflection.AssemblyProductAttribute("MISA.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MISA.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
